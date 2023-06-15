b0a1ec8127549b5d95c2d0729e57aecfae3ebeec
Shayan <ltaslim@montgomerycollege.edu>
2023-06-14 21:38:38 -0400

    fixed module issue

--- src/controllers/email.ts
+++ src/controllers/email.ts
@@ -24,15 +24,13 @@ class Email {
 		const mcEmail: IMCEmail = Email.convertEmail(email);
 
 		// send email through MailChannels
-		const resp = await fetch(
-			new Request('https://api.mailchannels.net/tx/v1/send', {
-				method: 'POST',
-				headers: {
-					'content-type': 'application/json',
-				},
-				body: JSON.stringify(mcEmail),
-			})
-		);
+		const resp = await fetch('https://api.mailchannels.net/tx/v1/send', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify(mcEmail),
+		});
 
 		// check if email was sent successfully
 		if (resp.status > 299 || resp.status < 200) {
